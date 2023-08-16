4d2ea4a9cca49767e96f2ee1efb98995aa4c8d5b
Marc Deroca <mvderoca@gmail.com>
2023-08-16 10:59:05 -0700

    second commit

--- book-management-app/src/services/bookService.js
+++ book-management-app/src/services/bookService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 // const API_URL = "http://localhost:5000/";
-const API_URL = "https://practicebackend-4ohm.onrender.com";
+const API_URL = "https://practicebackend-4ohm.onrender.com/";
 
 const getBooks = () => {
     return axios.get(API_URL, {headers:authHeader()});
